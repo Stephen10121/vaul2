3b5c40bbd041ffcafd1b6a802f397684396802a5
Stephen Gruzin <stephengrzn@gmail.com>
2024-01-26 23:22:52 -0800

    Polishing the component up.

--- src/lib/Drawer.svelte
+++ src/lib/Drawer.svelte
@@ -11,19 +11,70 @@
      */
     export let closeDrawer = false;
 
-    $: {
-        if (closeDrawer) {
-            close();
-            closeDrawer = false;
-        }
+    /**
+     * This value plus the the default top of the drawer position will determine when the drawer will close when the user drags the drawer beyond this value.
+     */
+    export let closeHeight = 200;
+
+    /**
+     * This value determines how far away the top of the drawer is from the top of the page.
+     */
+    export let fromTop = 40;
+
+    /**
+     * This value set the maximum the user can drag the drawer upwards.
+     */
+    export let userMinDragHeight = 20;
+
+    /**
+     * Sets the maximum opacity of the background when the drawer is moving. Max value: 129, Min value: 0.
+     */
+    export let maximumDark = 129;
+
+    /**
+     * Sets the minimim opacity of the background when the drawer is moving. Max value: 129, Min value: 0.
+     */
+    export let minimumDark = 20;
+
+    /**
+     * Set this to true if you want a close button in the header section.
+     */
+    export let showCloseButton = false;
+
+    /**
+     * If you want to add a title to the drawer header, set this to the title.
+     */
+    export let headerTitle: string | undefined = undefined;
+
+    /**
+     * Set a font for the header title.
+     */
+    export let headerFont: string | undefined = undefined;
+
+    /**
+     * This shows a bar in the header section indicating that the drawer is draggable.
+     */
+    export let showHeaderBar = false;
+
+    /**
+     * If you dont what this drawer to be draggable, set this to false.
+     */
+    export let isDraggable = true;
+
+    const dispatch = createEventDispatcher();
+    let backgroundColor = "#00000000";
+    let transitionTop = false;
+    let translateY = "100%";
+    let autoAnimate = true;
+    let oldPosOffset = 0;
+    let expand = false;
+    let top = fromTop;
+
+    function map_range(value: number, low1: number, high1: number, low2: number, high2: number) {
+        return Math.floor(low2 + (high2 - low2) * (value - low1) / (high1 - low1)).toString(16);
     }
 
-    let autoAnimate = true;
     $: actuallAnimate = animate === "transition" ? true : animate === "auto" ? autoAnimate : false;
-
-    let backgroundColor = "#00000000";
-    let translateY = "100%";
-    const dispatch = createEventDispatcher();
 
     function close() {
         backgroundColor = "#00000000";
@@ -33,48 +84,98 @@
         }, actuallAnimate ? 300 : 0);
     }
 
-    let top=40;
-    function moveMouse(event: MouseEvent) {
-        if (!expand) return;
-        top = Math.max(event.pageY-oldPosOffset, 20);
+    $: {
+        if (closeDrawer) {
+            close();
+            closeDrawer = false;
+        }
     }
-    let oldPosOffset = 0;
+
+    $: {
+        if (top !== fromTop) {
+            let minC = fromTop - userMinDragHeight;
+            let maxC = window.innerHeight;
+            backgroundColor = `#000000${map_range(top, minC, maxC, maximumDark, minimumDark)}`;
+        }
+    }
+
     function mouseDown(event:MouseEvent) {
         expand = true;
         oldPosOffset = event.offsetY;
     }
-    let transitionTop = false;
+
+    function touchDown(event: TouchEvent) {
+        expand = true;
+        oldPosOffset = event.targetTouches[0].clientY - document.querySelector("#inner")!.getBoundingClientRect()!.top;
+    }
+
+    function moveMouse(event: MouseEvent) {
+        if (!expand) return;
+        top = Math.max(event.pageY-oldPosOffset, fromTop-userMinDragHeight);
+    }
+
+    function moveTouch(event: TouchEvent) {
+        if (!expand) return;
+        top = Math.max(event.targetTouches[0].clientY - oldPosOffset, fromTop-userMinDragHeight);
+    }
+
+
     function mouseUp() {
         expand = false;
 
-        if (top > 100) {
+        if (top > (fromTop + closeHeight)) {
             close();
         } else {
-            transitionTop=true;
-            top = 40;
-            setTimeout(() => transitionTop=false, 150);
-            console.log(translateY)
+            transitionTop = actuallAnimate;
+            top = fromTop;
+            setTimeout(() => transitionTop=false, actuallAnimate ? 150 : 0);
         }
     }
 
     onMount(() => {
-        const isReduced = !(window.matchMedia(`(prefers-reduced-motion: reduce)`) as any as boolean === true || window.matchMedia(`(prefers-reduced-motion: reduce)`).matches === true);
-        autoAnimate = isReduced;
+        autoAnimate = !(window.matchMedia(`(prefers-reduced-motion: reduce)`) as any as boolean === true || window.matchMedia(`(prefers-reduced-motion: reduce)`).matches === true);
 
         setTimeout(() => {
             backgroundColor = "#00000081";
             translateY = "0%"
         }, actuallAnimate ? 1 : 0);
     });
-
-    let expand = false;
 </script>
 
-<section class="outer {actuallAnimate ? "animate" : ""}" style="--backgroundColor:{backgroundColor};--translateY:{translateY};--topPos:{top}px;">
+<section
+    class="outer {actuallAnimate ? "animate" : ""}"
+    style="--backgroundColor:{backgroundColor};--translateY:{translateY};--topPos:{top}px;--topPosConst:{fromTop}px;{headerFont ? `--font:${headerFont}`: ""}"
+>
     <button class="closeBackground" on:click={close} />
-    <section class="inner {transitionTop && "transitionTop"}">
+    <section class="inner {transitionTop && "transitionTop"}" id="inner">
         <div class="header">
-            <div class="slider {expand && "expand"}" on:mousedown={mouseDown} on:mouseup={mouseUp} on:mousemove={moveMouse} on:mouseleave={mouseUp} role="none" />
+            {#if headerTitle}
+                <h2>{headerTitle}</h2>
+            {/if}
+            {#if showHeaderBar}
+                <div class="bar"></div>
+            {/if}
+            {#if showCloseButton}
+                <button class="closeButton" on:click={close} title="Close Drawer">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
+                        <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
+                    </svg>
+                </button>
+            {/if}
+            {#if isDraggable}
+                <!-- svelte-ignore a11y-no-static-element-interactions -->
+                <div
+                    class="slider {expand && "expand"} {showCloseButton && "closeButtonShowing"}"
+                    role="none"
+                    on:touchstart={touchDown}
+                    on:touchmove={moveTouch}
+                    on:touchend={mouseUp}
+                    on:mousedown={mouseDown}
+                    on:mousemove={moveMouse}
+                    on:mouseleave={mouseUp}
+                    on:mouseup={mouseUp}
+                />
+            {/if}
         </div>
         <div class="rest">
             <slot />
@@ -107,12 +208,19 @@
         position: absolute;
         left: 0;
         top: var(--topPos);
-        height: calc(100% - 40px);
+        height: calc(100% - var(--topPosConst));
         width: 100%;
         background-color: white;
         transform: translateY(var(--translateY));
         border-top-right-radius: 10px;
         border-top-left-radius: 10px;
+        overflow: visible !important;
+    }
+
+    .rest {
+        width: 100%;
+        height: calc(100% - 40px);
+        overflow-y: auto;
     }
 
     .outer.animate .inner.transitionTop {
@@ -125,7 +233,7 @@
 
     .closeBackground {
         width: 100%;
-        height: 40px;
+        height: var(--topPosConst);
         border: none;
         background: none;
         outline: none;
@@ -135,20 +243,75 @@
         width: 100%;
         height: 40px;
         position: relative;
+        overflow: visible !important;
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+        padding: 0 10px;
+    }
+
+    .header h2 {
+        font-family: var(--font, sans-serif);
+        font-size: 1.25rem;
+        font-weight: bold;
     }
 
     .slider {
-        background-color: rgba(255, 0, 0, 0.349);
         width: 100%;
         height: 100%;
         position: absolute;
         top: 0;
         left: 0;
+        z-index: 200;
+    }
+
+    .slider.closeButtonShowing {
+        width: calc(100% - 40px);
+    }
+
+    .closeButton:hover svg {
+        fill: #696969;
+    }
+
+    .closeButton {
+        position: absolute;
+        top: 0;
+        right: 20px;
+        width: 40px;
+        height: 40px;
+        border: none;
+        background: none;
+        cursor: pointer;
     }
 
     .slider.expand {
-        /* transform: translateY(-10px); */
         transform: translateY(-600px);
         height: 3000%;
+    }
+
+    :global(*:has(.slider.slider.expand)) {
+        overflow: hidden;
+        -webkit-user-select: none;
+        -khtml-user-select: none;
+        -moz-user-select: none;
+        -o-user-select: none;
+        user-select: none;
+        -webkit-user-drag: none;
+        -khtml-user-drag: none;
+        -moz-user-drag: none;
+        -o-user-drag: none;
+        user-drag: none;
+        overscroll-behavior: none;
+    }
+
+    .bar {
+        width: 50px;
+        height: 6px;
+        background-color: #cccccc;
+        border-radius: 100vw;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
     }
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import Drawer from "$lib/Drawer.svelte";
 
-    let showModal = true;
+    let showModal = false;
     let closeModalForce = false;
 
     function closeModal() {
@@ -10,12 +10,38 @@
     }
 </script>
 
-<button on:click={() => showModal = true}>Open</button>
-
+<main>
+    <button on:click={() => showModal = true}>Open</button>
+</main>
 
 {#if showModal}
-    <Drawer on:close={() => showModal = false} closeDrawer={closeModalForce}>
-        wow
-        <button on:click={closeModal}>Close</button>
+    <Drawer on:close={() => showModal = false} closeDrawer={closeModalForce} showHeaderBar>
     </Drawer>
 {/if}
+
+<style>
+    main {
+        width: 100%;
+        height: 100vh;
+        height: 100dvh;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    button {
+        padding: 10px 17px;
+        font-family: 'Open Sans', sans-serif;
+        font-size: 0.75rem;
+        font-weight: bold;
+        border-radius: 100vw;
+        border: 1px solid #adadad;
+        background: none;
+        cursor: pointer;
+        transition: background 0.15s linear;
+    }
+
+    button:hover {
+        background-color: #f7f7f7;
+    }
+</style>
